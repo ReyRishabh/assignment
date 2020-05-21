0a3db9fac6064ca817a1a8ef7da5a8ac6216631a
Rishabh Rajpal <14rishab@gmail.com>
2020-05-21 11:41:53 +0530

    assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,70 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'apptunixAssignment';
+  title = 'Apptunix Assignment';
+  allBoxes = [
+    {
+      name: "box 1",
+      img: "assets/one.png",
+      id: 1
+    },
+    {
+      name: "box 2",
+      img: "assets/two.jpeg",
+      id: 2
+    },
+    {
+      name: "box 3",
+      img: "assets/three.png",
+      id: 3
+    },
+    {
+      name: "box 4",
+      img: "assets/four.png",
+      id: 4
+    },
+    {
+      name: "box 5",
+      img: "assets/five.png",
+      id: 5
+    },
+    {
+      name: "box 6",
+      img: "assets/six.jpg",
+      id: 6
+    }
+  ];
+  selectedBox;
+  selectBox(box, index) {
+    console.log("on delete");
+    let array = [];
+    array = this.allBoxes.filter(element => {
+      return element.img === "";
+    });
+    if (array.length) {
+      for (var i = index - 1; i > 0; i--) {
+        if (this.allBoxes[i].img) {
+          this.allBoxes[i + 1].img = box.img;
+          this.selectedBox = this.allBoxes[i + 1];
+          this.allBoxes[index].img = "";
+          break;
+        }
+      }
+    } else {
+      this.selectedBox = box;
+    }
+  }
+  deleteImage(event, index) {
+    this.allBoxes[index].img = "";
+    for (var i = index + 1; i < this.allBoxes.length; i++) {
+      if (this.allBoxes[i].img) {
+        this.allBoxes[i - 1].img = this.allBoxes[i].img;
+      }
+      this.allBoxes[i].img = "";
+    }
+    console.log(this.allBoxes, "///");
+  }
+  onEvent(event) {
+    event.stopPropagation();
+  }
 }
